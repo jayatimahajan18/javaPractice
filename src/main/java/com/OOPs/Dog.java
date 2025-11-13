2c2dfbd7cea36ba772d07b14744e8945be9a8bef
jayatimahajan18 <jayatimahajan18@gmail.com>
2025-11-13 15:39:50 +0530

    Add access modifier and getter and setter Update main() in DigTest.java

--- src/main/java/com/OOPs/Dog.java
+++ src/main/java/com/OOPs/Dog.java
@@ -2,33 +2,69 @@ package com.OOPs;
 
 public class Dog {
 
-    int legs;
-    int eyes;
-    String nameOfDog;
-    String colour;
-    float weight;
-    public Dog(int legs, int eyes, String nameOfDog, String colour, float weight){
-        this.legs=legs;
-        this.eyes=eyes;
-        this.nameOfDog=nameOfDog;
-        this.colour=colour;
-        this.weight=weight;
+    private int legs;
+    private int eyes;
+    private String nameOfDog;
+    private String colour;
+    private float weight;
+
+    public Dog(int legs, int eyes, String nameOfDog, String colour, float weight) {
+        this.legs = legs;
+        this.eyes = eyes;
+        this.nameOfDog = nameOfDog;
+        this.colour = colour;
+        this.weight = weight;
     }
 
-    public int howManyLegs(){
+    public int howManyLegs() {
+        return getLegs();
+    }
+
+    public String dogColour() {
+        return getColour();
+    }
+
+    public String dogName() {
+        return getNameOfDog();
+    }
+
+    public float dogWeight() {
+        return getWeight();
+    }
+
+    public int dogEyes() {
+        return geteyes();
+    }
+
+    public int getLegs(){
         return this.legs;
     }
-    public String dogColour(){
-        return this.colour;
+    public void setLegs(int legs){
+        this.legs=legs;
     }
-    public String dogName(){
+    public int geteyes(){
+        return this.eyes;
+    }
+    public void seteyes(int eyes){
+        this.eyes=eyes;
+    }
+    public String getNameOfDog(){
         return this.nameOfDog;
     }
-    public float dogWeight(){
+    public void setNameOfDog(String nameOfDog){
+        this.nameOfDog=nameOfDog;
+    }
+    public String getColour(){
+        return this.colour;
+    }
+    public void setColour(String colour){
+        this.colour=colour;
+    }
+    public float getWeight(){
         return this.weight;
     }
-    public int dogEyes(){
-        return this.eyes;
+    public void setWeight(float weight){
+        this.weight=weight;
     }
 
 }
